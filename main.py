154d01cf09e628389b62d360615148faa75eb54b
Omar Mejia <topesa@gmail.com>
2023-12-12 22:42:14 -0700

    [ADD] se agrega proyecto de fastapi_htmx

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Configuración CORS para permitir solicitudes desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+conteo_rapido = "pages/conteo_rapido/"
+# Configuración de Jinja2
+# Asegúrate de tener un directorio 'templates' en tu proyecto
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/fast_consumption")
+async def fast_consumption():
+    return FileResponse("templates/pages/conteo_rapido/fast_consumption.html")
+
+
+@app.get("/cargar_tabla", response_class=HTMLResponse)
+def cargar_tabla(request: Request):
+    return templates.TemplateResponse(conteo_rapido + "table_partial.html", {"request": request})
