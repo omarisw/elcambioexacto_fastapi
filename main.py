c54cfa66c0191bdd158c00f126b9a7316ebaf50d
Omar Mejia <topesa@gmail.com>
2023-12-13 00:30:42 -0700

    [add] se agregan tablas optimizadas

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+
+class ValorModel(BaseModel):
+    valor: int
+
 
 app = FastAPI()
 
@@ -20,6 +26,20 @@ conteo_rapido = "pages/conteo_rapido/"
 templates = Jinja2Templates(directory="templates")
 
 
+# Datos de ejemplo
+colores = [
+    {"nombre": "Lácteos", "valor": 1},
+    {"nombre": "Verduras", "valor": 2},
+    {"nombre": "Frutas", "valor": 3},
+    {"nombre": "Granos y Cereales", "valor": 4},
+    {"nombre": "Carnes", "valor": 5},
+    {"nombre": "Grasas", "valor": 6},
+    {"nombre": "Libres", "valor": 7},
+    {"nombre": "Agua", "valor": 8},
+    {"nombre": "Puntos extras", "valor": 9},
+]
+
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -35,11 +55,9 @@ def cargar_tabla(request: Request):
     return templates.TemplateResponse(conteo_rapido + "table_partial.html", {"request": request})
 
 
-@app.post("/tablita", response_class=HTMLResponse)
-def carga_tablita(request: Request):
-    return templates.TemplateResponse(conteo_rapido + "colores.html", {"request": request})
-
-
-@app.post("/tablita2", response_class=HTMLResponse)
-def carga_tablita2(request: Request):
-    return templates.TemplateResponse(conteo_rapido + "colores2.html", {"request": request})
+@app.get("/tablita/{valor_id}", response_class=HTMLResponse)
+def carga_tablita(request: Request, valor_id: int):
+    # Filtrar colores
+    colores_filtrados = [
+        color for color in colores if color["valor"] == valor_id]
+    return templates.TemplateResponse(conteo_rapido + "colores.html", {"request": request, "colores": colores_filtrados})
